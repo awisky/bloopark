49e45900d86d9fd410b82cb2cb7dd2084f09c381
Agustin Wisky <agustinwisky@MacBook-de-Agustin.local>
2020-04-06 23:10:33 -0300

    [NEW][product_rank] Product Rank Module

---
+++ addons/product_rank/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Test Case For Bloopark by Agustin Wisky <agustinwisky@gmail.com>
+
+{
+    'name': "Product Rank",
+    'summary': """
+        Product Rank """,
+    'description': """
+        Bloopark Test Case
+        Product Rank
+    """,
+    'author': "Agustin Wisky <agustinwisky@gmail.com>",
+    'website': "https://agustinwisky.com",
+    'category': 'Proyect',
+    'version': '13.0.1.0',
+    'depends': [
+        'sale',
+        'purchase',
+        'search_assistant',
+    ],
+    'data': [
+
+    ],
+    'installable': True,
+}
